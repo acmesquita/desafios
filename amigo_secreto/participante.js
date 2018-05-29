c1c6d55ac8a733ec64c8a9aaa7466be48faa3987
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2018-05-29 17:11:03 -0300

    Desafio amigo secreto em Javascript

---
+++ amigo_secreto/participante.js
@@ -0,0 +1,6 @@
+class Participante{
+	constructor(nome){
+		this.nome = nome;
+		this.amigo = null;
+	}
+}
---
+++ amigo_secreto/secret_friend.js
@@ -0,0 +1,30 @@
+class AmigoSecreto{
+
+	constructor(participantes){
+		this.participantes = participantes;
+	}
+
+	sortear() {
+		let lista = this.shuffle(this.participantes);
+		for (var i = 0; i < lista.length; ++i) {
+			const amigo = i == lista.length-1 ? lista[0] : lista[i+1];
+			lista[i].amigo = amigo;
+		}
+		this.imprimirParticipantes(lista);
+		return lista;
+	}
+
+	imprimirParticipantes(lista){
+		lista.forEach(p => console.log(p.nome + " tirou " + p.amigo.nome));
+	}
+
+	shuffle(lista) {	
+		for (var i = lista.length - 1; i >= 0; i--) {
+			let j = Math.floor(Math.random()*(i+1));
+			let tmp = lista[i];
+			lista[i] = lista[j];
+			lista[j] = tmp;
+		}
+		return lista;
+	}
+}
