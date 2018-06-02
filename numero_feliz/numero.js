ace813c6bbef0aabbee8bcf0dbdebface72ec115
Catharina Mesquita <catha.ana.1994@gmail.com>
2018-06-02 10:45:53 -0300

    Finalizando a implementação do desafio

--- numero_feliz/numero.js
+++ numero_feliz/numero.js
@@ -2,26 +2,30 @@ class Numero{
 
 	constructor(numero){
 		this.numero = numero;
+		this.arrayNmeros = []
 	}
 
 	isNumeroFeliz(numero = this.numero){
 		let msg = "";
-		if (numero == 2){
+		if (this.arrayNmeros.includes(numero)){
 			msg = `${this.numero} é um número triste =(`;
 		}else if (numero == 1){
 			msg = `${this.numero} é um número feliz =D`;
 		}else{
+			this.arrayNmeros.push(numero);
 			let numeros = Array.from(numero.toString());
-			console.log(numeros);
+			// console.log(numeros);
 			let arrayDobrado = numeros.map(e => Math.pow(e, 2));
-			console.log(arrayDobrado);
+			// console.log(arrayDobrado);
 			let novoNumero = arrayDobrado.reduce((a,b) => {return a+b;}, 0);
-			console.log(novoNumero);
+			// console.log(novoNumero);
 			return this.isNumeroFeliz(novoNumero);
 		}
 	    return msg;
 	}
 }
 
-var n = new Numero(7)
-n.isNumeroFeliz();
+// var n = new Numero(7)
+// var msg = n.isNumeroFeliz();
+
+// console.log(msg)
