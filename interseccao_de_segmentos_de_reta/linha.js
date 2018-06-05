108142a90ae0d7e6cc0de418d4ab387e4feedbfa
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2018-06-05 14:51:39 -0300

    Criando estrutura para o desafio

---
+++ interseccao_de_segmentos_de_reta/linha.js
@@ -0,0 +1,11 @@
+class Linha {
+    constructor(partida, destino){
+        this.partida = partida;
+        this.destino = destino;
+    }
+
+    intersecta(outra_linha){
+        // TODO
+    }
+
+}
